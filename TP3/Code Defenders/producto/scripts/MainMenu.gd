bbef2963ef2396ceb69e03029b6438f7557e7189
MouGliXx <97257192+MouGliXx@users.noreply.github.com>
2023-05-15 09:53:58 -0300

    Controls Menu

--- TP3/Code Defenders/producto/scripts/MainMenu.gd
+++ TP3/Code Defenders/producto/scripts/MainMenu.gd
@@ -1,17 +1,7 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Music.play(55.0)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_StartButton_pressed():
 	$Transition.next("res://LevelMenu.tscn")
